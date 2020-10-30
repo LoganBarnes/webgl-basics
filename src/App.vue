fdea1666825e7594cb32c3e0dec64967d04c1739
Logan Barnes <logan@cloudnc.com>
2020-10-30 01:58:51 +0000

    Add canvas and set gl clear color

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <GLCanvas ref="canvas" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import GLCanvas from "./components/GLCanvas.vue";
+import { vec2 } from "gl-matrix";
 
 @Options({
   components: {
-    HelloWorld
+    GLCanvas
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private viewport: vec2 = vec2.create();
+
+  public mounted(): void {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+    this.buildGLPipeline();
+    this.renderGLPipeline();
+  }
+
+  public beforeDestroy(): void {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  public handleResize(): void {
+    this.viewport = (this.$refs.canvas as GLCanvas).resize();
+  }
+
+  private buildGLPipeline(): void {
+    const gl: WebGLRenderingContext = (this.$refs.canvas as GLCanvas).glContext;
+
+    gl.clearColor(1.0, 0.5, 0.1, 1.0);
+  }
+
+  private renderGLPipeline(): void {
+    const gl: WebGLRenderingContext = (this.$refs.canvas as GLCanvas).glContext;
+
+    /* tslint:disable:no-bitwise */
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    /* tslint:enable:no-bitwise */
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  }
+}
 </script>
 
 <style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/GLCanvas.vue
@@ -0,0 +1,60 @@
+<template>
+  <canvas id="canvas" tabindex="1">
+    HTML5 canvas is not supported in this browser :(
+  </canvas>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { vec2 } from "gl-matrix";
+
+export default class GLCanvas extends Vue {
+  private gl: null | WebGLRenderingContext = null;
+
+  /**
+   * Initializes WebGL when the canvas is ready.
+   */
+  public mounted(): void {
+    const canvas: HTMLCanvasElement = this.$el as HTMLCanvasElement;
+    const options: WebGLContextAttributes = { preserveDrawingBuffer: true };
+    const gl = canvas.getContext("webgl", options);
+
+    if (!gl) {
+      throw new Error("Failed to initialise WebGL");
+    }
+    this.gl = gl;
+
+    canvas.focus();
+
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+  }
+
+  /**
+   * Adjust the canvas when the window is resized
+   */
+  public resize(): vec2 {
+    const canvas: HTMLCanvasElement = this.$el as HTMLCanvasElement;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    console.log("resize");
+
+    return vec2.fromValues(canvas.width, canvas.height);
+  }
+
+  get glContext(): WebGLRenderingContext {
+    return this.gl as WebGLRenderingContext;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+  /*fill container completely */
+  width: 100%;
+  height: 100%;
+  background-color: black; /*ideally matches WebGL clear color*/
+}
+</style>
